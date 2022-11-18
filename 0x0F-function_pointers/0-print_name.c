385d064c0b32446eee8b3e7eee5e8921dd2461d8
Vickslim <113726585+Vickslim@users.noreply.github.com>
2022-11-17 18:57:57 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,13 @@
-vch
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: the name
+ * @f: functio pointer
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
